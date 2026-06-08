fe8600a23fd274260f917e3689e51687edb5324c
Mad Mex <74403807+LordMrMittens@users.noreply.github.com>
2026-06-08 10:33:01 +0100

    Added ABPs and IMCs

---
+++ Source/Aura/Private/Characters/Player/AuraPlayerController.cpp
@@ -0,0 +1,16 @@
+// Copyright Masters
+
+
+#include "Characters/Player/AuraPlayerController.h"
+#include "AuraPlayerController.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
