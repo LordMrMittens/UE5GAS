ac5d93bdb0979736f3b99853d3f1befa33711a0a
Mad Mex <74403807+LordMrMittens@users.noreply.github.com>
2026-06-17 10:01:09 +0100

    Progress bars and HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+// Copyright Masters
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widgets/AuraUserWidget.h"
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
